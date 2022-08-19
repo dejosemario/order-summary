9468ea54a6a0c4889ead5b7c79e825c8fa59f64a
dejosemario <josemariaodumodu@gmail.com>
2022-08-19 14:54:21 +0100

    Develop the mobile view

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="app__container">
+          <img src="../images/illustration-hero.svg" alt="illustration..."
+          className='hero' />
+          <div className="body__message">
+            <h2>Order Summary </h2>
+            <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
+            <div className="price__box">
+              <img src="../images/icon-music.svg" alt="Icon-music" />
+              <div className="annual__plan__box">
+                <p>Annual Plan</p>
+                <span className='price'>$59.99/year</span>
+              </div>
+              <a href="chngeme.com" className='anchor'>Change</a>
+            </div>
+            <button>Proceed to Payment</button>
+            <span>Cancel Order</span>
+          </div>
+      </div>
     </div>
   );
 }
